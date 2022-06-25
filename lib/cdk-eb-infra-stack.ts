22cd445d0643a9292a0eb8a6e2c8f891d648a58c
Arundeep Chohan <arundeepchohan2009@hotmail.com>
2022-06-25 14:48:49 -0700

    Adding files

---
+++ lib/cdk-eb-infra-stack.ts
@@ -0,0 +1,86 @@
+import {
+  Stack,
+  StackProps,
+  aws_elasticbeanstalk as elasticbeanstalk,
+  aws_iam as iam,
+} from "aws-cdk-lib";
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3a from 'aws-cdk-lib/aws-s3-assets';
+export class CdkEbInfraStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+    // Construct an S3 asset from the ZIP located from directory up.
+    const webAppZipArchive = new s3a.Asset(this, 'WebAppZip', {
+      path: `${__dirname}/../app.zip`,
+    });
+    // Create a ElasticBeanStalk app.
+    const appName = 'MyWebApp';
+    const app = new elasticbeanstalk.CfnApplication(this, 'Application', {
+        applicationName: appName,
+    });
+    // Create an app version from the S3 asset defined earlier
+    const appVersionProps = new elasticbeanstalk.CfnApplicationVersion(this, 'AppVersion', {
+      applicationName: appName,
+      sourceBundle: {
+          s3Bucket: webAppZipArchive.s3BucketName,
+          s3Key: webAppZipArchive.s3ObjectKey,
+      },
+    });
+    // Make sure that Elastic Beanstalk app exists before creating an app version
+    appVersionProps.addDependsOn(app);
+    // Create role and instance profile
+    const myRole = new iam.Role(this, `${appName}-aws-elasticbeanstalk-ec2-role`, {
+      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+    });
+
+    const managedPolicy = iam.ManagedPolicy.fromAwsManagedPolicyName('AWSElasticBeanstalkWebTier')
+    myRole.addManagedPolicy(managedPolicy);
+
+    const myProfileName = `${appName}-InstanceProfile`
+
+    const instanceProfile = new iam.CfnInstanceProfile(this, myProfileName, {
+      instanceProfileName: myProfileName,
+      roles: [
+          myRole.roleName
+      ]
+    });
+    // Example of some options which can be configured
+    const optionSettingProperties: elasticbeanstalk.CfnEnvironment.OptionSettingProperty[] = [
+      {
+          namespace: 'aws:autoscaling:launchconfiguration',
+          optionName: 'IamInstanceProfile',
+          value: myProfileName,
+      },
+      {
+          namespace: 'aws:autoscaling:asg',
+          optionName: 'MinSize',
+          value: '1',
+      },
+      {
+          namespace: 'aws:autoscaling:asg',
+          optionName: 'MaxSize',
+          value: '1',
+      },
+      {
+          namespace: 'aws:ec2:instances',
+          optionName: 'InstanceTypes',
+          value: 't2.micro',
+      },
+    ];
+    // Create an Elastic Beanstalk environment to run the application
+    const elbEnv = new elasticbeanstalk.CfnEnvironment(this, 'Environment', {
+      environmentName: 'MyWebAppEnvironment',
+      applicationName: app.applicationName || appName,
+      solutionStackName: '64bit Amazon Linux 2 v5.5.4 running Node.js 14',
+      optionSettings: optionSettingProperties,
+      versionLabel: appVersionProps.ref,
+    });
+    // example resource
+    // const queue = new sqs.Queue(this, 'CdkEbInfraQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
---
+++ test/cdk-eb-infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkEbInfra from '../lib/cdk-eb-infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-eb-infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkEbInfra.CdkEbInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
